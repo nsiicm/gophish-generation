f87991c63888fee0342426935f986b4d74354c7c
nicolas.simon.watt@gmail.com <nicolas.simon.watt@gmail.com>
2020-12-29 17:31:36 +0100

    basic argument parser finished

--- generate.py
+++ generate.py
@@ -2,7 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Creation de rapport avec exports Gophish') #Creation du parser d'arguments et ajout description
 
-parser.add_argument("--raw-events", help="Indiquer le fichier d'export Gophish Raw Events")
-parser.add_argument("--results", help="Indiquer le fichier d'export Gophish Resulsts")
+parser.add_argument("-e","--raw-events", help="Indiquer le fichier d'export Gophish Raw Events", required=True)
+parser.add_argument("-r","--results", help="Indiquer le fichier d'export Gophish Resulsts", required=True)
+parser.add_argument("-v","--verbose", action="store_true", help="Afficher les details de traitement", required=False)
 
 args = parser.parse_args()
