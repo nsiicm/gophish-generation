f3038cdd45a821bdc6c987014338f29dcf3d1735
nicolas.simon.watt@gmail.com <nicolas.simon.watt@gmail.com>
2020-12-29 17:46:42 +0100

    Creation de l classe utilisateur et test

--- generate.py
+++ generate.py
@@ -1,9 +1,15 @@
 import argparse
+from user import User
 
 parser = argparse.ArgumentParser(description='Creation de rapport avec exports Gophish') #Creation du parser d'arguments et ajout description
 
-parser.add_argument("-e","--raw-events", help="Indiquer le fichier d'export Gophish Raw Events", required=True)
-parser.add_argument("-r","--results", help="Indiquer le fichier d'export Gophish Resulsts", required=True)
+parser.add_argument("-e","--raw-events", help="Indiquer le fichier d'export Gophish Raw Events", required=False)
+parser.add_argument("-r","--results", help="Indiquer le fichier d'export Gophish Resulsts", required=False)
 parser.add_argument("-v","--verbose", action="store_true", help="Afficher les details de traitement", required=False)
 
 args = parser.parse_args()
+
+
+utilisateur = User("SIMON", "Nicolas", "n@a.a", "sdlfkjsqldfj")
+
+print(utilisateur.nom)
---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, nom, prenom, email, id):
+        self.nom=nom
+        self.prenom=prenom
+        self.email=email
+        self.id=id
