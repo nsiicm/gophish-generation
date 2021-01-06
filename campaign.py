7aa0d3f8e959153f1755fbc46a314a6ebfd3a301
nicolas.simon.watt@gmail.com <nicolas.simon.watt@gmail.com>
2021-01-06 11:12:09 +0100

    email render

--- campaign.py
+++ campaign.py
@@ -1,4 +1,5 @@
-
+import jinja2
+from jinja2 import Template
 import datetime
 import imgkit
 import pdfkit
@@ -18,6 +19,9 @@ class Campaign:
         self.landing_credentials = campaign["page"]["capture_credentials"]
         self.landing_passwords = campaign["page"]["capture_passwords"]
         self.landing_redirect = campaign["page"]["redirect_url"]
+        self.mail_html = campaign["template"]["html"]
+        self.mail_name = campaign["template"]["name"]
+        self.mail_subject = campaign["template"]["subject"]
         self.launch_date = campaign["launch_date"]
         self.send_by_date = campaign["send_by_date"]
         self.completed_date = campaign["completed_date"]
@@ -27,7 +31,10 @@ class Campaign:
         serialized = {}
         serialized['name'] = self.name
         serialized['landing_name'] = self.landing_name
-        serialized['landing_html_rendered'] = self.render_html()
+        serialized['landing_html_rendered'] = self.render_html(self.landing_html)
+        serialized['email_html_rendered'] = self.render_email()
+        serialized['email_name'] = self.mail_name
+        serialized['email_subject'] = self.mail_subject
         serialized['redirect'] = self.landing_redirect
         if self.landing_credentials:
             serialized['credentials'] = 'Oui'
@@ -43,13 +50,33 @@ class Campaign:
         serialized['url'] = self.url
         return serialized
 
-    def render_html(self):
+    def render_email(self):
+        date = datetime.datetime.now()
+        filename = date.strftime("%f") + '.jinja2'
+        path = './template/' + filename
+        f = open(os.path.abspath(path), "a")
+        f.write(self.mail_html)
+        f.close()
+        latex_jinja_env = jinja2.Environment(
+        variable_start_string = '{{.',
+    	variable_end_string = '}}',
+	    trim_blocks = True,
+	    autoescape = False,
+	    loader = jinja2.FileSystemLoader(os.path.abspath('./template'))
+        )
+        template = latex_jinja_env.get_template(filename)
+        html = template.render({'FirstName' : 'FirstName', "LastName" : "LastName", "Email" : "lastname@domain.com", "Position" : "position", "RId" : "Rid", "From":"From"})
+        os.remove(os.path.abspath(path))
+        return self.render_html(html)
+
+    def render_html(self,html):
         date = datetime.datetime.now()
         filename =  date.strftime("%f") + ".png"
         path = './latex/' + filename
         path_wkhtmltoimage = 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltoimage.exe'
         config = imgkit.config(wkhtmltoimage=path_wkhtmltoimage)
-        options = {'format':'png','quality':'25'}
+        options = {'enable-local-file-access': ""}
         #options = {'enable-local-file-access': True}
-        imgkit.from_string(self.landing_html,os.path.abspath(path),config=config,options=options)
+        imgkit.from_string(html,os.path.abspath(path),config=config,options=options)
         return filename
+
